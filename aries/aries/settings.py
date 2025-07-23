d4551d7e138e7efe9350e214cd70ca3fd35cd698
INPHINITHY <dwomohaustin14@gmail.com>
2025-07-22 21:43:58 -0500

    Update settings.py

--- aries/aries/settings.py
+++ aries/aries/settings.py
@@ -135,7 +135,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
       # Path to your static files folder
 ]
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
